75a01f4241f6132fd1a90c959cc74a4c43c8233c
Edward <programmeredward@gmail.com>
2024-01-13 10:17:31 +0800

    Initialized Postgresql and connected to server

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import pool from './database/db.js';
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.get('/test', async (req, res) => {
+    try {
+        const results = await pool.query("SELECT * FROM users")
+        res.json(results.rows)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`);
