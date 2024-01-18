0a6251cde4a07de0a68d011e6e4cfc7927693221
Edward <programmeredward@gmail.com>
2024-01-18 21:34:28 +0800

    Added initial UI for the country page

--- client/src/pages/Country.jsx
+++ client/src/pages/Country.jsx
@@ -6,19 +6,45 @@ export default function Country() {
     const { country } = useParams()
 
     const location = useGetLocation('country', country)
+    console.log(location)
    
   return (
-    <div>
-      <h1>
-        {location.continent}
-      </h1>
-      <h1>
-        {location.capital}
-      </h1>
-      <h1>
-        {location.id}
-      </h1>
-        
+    <div className="flex items-center flex-col gap-5">
+      <div className="">
+        <img 
+          className="object-cover rounded-2xl w-[960px]"
+          src={location.photo} 
+          alt="cover photo" 
+        />
+        <div className="flex mt-9">
+          <div>
+            <p className="text-6xl">
+              {location.country} 
+                <span className="text-2xl"> {location.continent}</span>
+            </p>
+            <p className="mt-9">
+              {location.description}
+            </p>
+            <p className="hover:underline hover:cursor-pointer text-blue-600">
+              Start a plan
+            </p>
+          </div>
+          <p>Check the cheapest flights, add flights component here</p>
+        </div>
+      </div>
+
+      <div>
+        Must See attractions. Add the notable places here
+      </div>
+
+      <div>
+        Must Read blogs. Add related blogs here
+      </div>
+
+      <div>
+        Check out user's reviews. Add reviews
+      </div>
+      
     </div>
   )
 }
