438ebbdbe7d6a5e18d166935d79493a70c281bab
Edward <programmeredward@gmail.com>
2024-01-19 18:24:19 +0800

    Updated country page UI

--- client/src/pages/Country.jsx
+++ client/src/pages/Country.jsx
@@ -1,50 +1,54 @@
 import { useParams } from "react-router-dom"
-import { useState } from "react"
-import useGetLocation from "../hooks/useGetLocation"
+import useGetCountry from "../hooks/useGetCountry"
 
 export default function Country() {
     const { country } = useParams()
-
-    const location = useGetLocation('country', country)
-    console.log(location)
+    const { chosenCountry, loading } = useGetCountry(country)
+    console.log(chosenCountry)
    
   return (
-    <div className="flex items-center flex-col gap-5">
-      <div className="">
-        <img 
-          className="object-cover rounded-2xl w-[960px]"
-          src={location.photo} 
-          alt="cover photo" 
-        />
-        <div className="flex mt-9">
-          <div>
-            <p className="text-6xl">
-              {location.country} 
-                <span className="text-2xl"> {location.continent}</span>
-            </p>
-            <p className="mt-9">
-              {location.description}
-            </p>
-            <p className="hover:underline hover:cursor-pointer text-blue-600">
-              Start a plan
-            </p>
+    <>
+    {loading ? <p className="text-3xl">
+      Loading...
+    </p> :
+      <div className="flex items-center flex-col gap-5">
+        <div className="">
+          <img 
+            className="object-cover rounded-2xl w-[960px]"
+            src={chosenCountry.photo} 
+            alt="cover photo" 
+          />
+          <div className="flex mt-9">
+            <div>
+              <p className="text-6xl">
+                {chosenCountry.country} 
+                  <span className="text-2xl"> {chosenCountry.continent_name}</span>
+              </p>
+              <p className="mt-9">
+                {chosenCountry.description}
+              </p>
+              <p className="hover:underline hover:cursor-pointer text-blue-600">
+                Start a plan
+              </p>
+            </div>
+            <p>Check the cheapest flights, add flights component here</p>
           </div>
-          <p>Check the cheapest flights, add flights component here</p>
         </div>
-      </div>
 
-      <div>
-        Must See attractions. Add the notable places here
-      </div>
+        <div>
+          Must See attractions. Add the notable places here
+        </div>
 
-      <div>
-        Must Read blogs. Add related blogs here
-      </div>
+        <div>
+          Must Read blogs. Add related blogs here
+        </div>
 
-      <div>
-        Check out user's reviews. Add reviews
+        <div>
+          Check out user's reviews. Add reviews
+        </div>
+
       </div>
-      
-    </div>
+      }
+    </>
   )
 }
