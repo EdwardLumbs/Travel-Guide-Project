232670caa307cd53fd98b46f1447a3b3e94ac654
Edward <programmeredward@gmail.com>
2024-01-19 15:50:08 +0800

    Updated SQL schema and added continent DB

--- api/database/queries/destinationQueries.sql
+++ api/database/queries/destinationQueries.sql
@@ -2,10 +2,20 @@ CREATE TABLE countries (
     id SERIAL PRIMARY KEY,
     country VARCHAR(255) NOT NULL,
     capital VARCHAR(255),
-    continent VARCHAR(20) NOT NULL,
+    continent_id INT REFERENCES continents(id),
     photo TEXT,
     description TEXT
 )
 
--- get counties
+CREATE TABLE continents (
+    id SERIAL PRIMARY KEY,
+    continent_name VARCHAR(20) NOT NULL,
+    photo TEXT,
+    description TEXT
+)
+
+-- get countries
 SELECT * FROM countries WHERE country ILIKE country
+
+-- get continents
+SELECT * FROM continents WHERE continent_name ILIKE continent
