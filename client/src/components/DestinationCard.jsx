7776989721671038557031a53bedb4b7fe4087f4
Edward <programmeredward@gmail.com>
2024-01-20 13:37:56 +0800

    Created initial UI for destination card

--- client/src/components/DestinationCard.jsx
+++ client/src/components/DestinationCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function DestinationCard() {
+export default function DestinationCard({photo, name}) {
   return (
-    <div>
-      
+    <div className='bg-white shadow-md hover:shadow-lg 
+    transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
+      <img 
+        className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
+        src={photo} 
+        alt="Cover Image" 
+      />
+      <p>
+        CONTINENT, CHANGE LATER
+      </p>
     </div>
   )
 }
