e2bdcc08d61b28fed4a74c565c0f180ff1ffbd40
Edward <programmeredward@gmail.com>
2024-01-24 21:10:36 +0800

    Added flight search functionality on country page depending on the IATA of the user

--- client/src/pages/Country.jsx
+++ client/src/pages/Country.jsx
@@ -1,12 +1,67 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useGetCountry from "../hooks/useGetCountry";
+import { useEffect, useState } from "react";
+import { useSelector } from 'react-redux';
 
 export default function Country() {
   // add a function where if continent and country arent validate, return error
-    const { countryName } = useParams();
-    console.log(countryName)
-    const { country, loading, countryError } = useGetCountry(countryName);
-    console.log(country);
+  const { countryName } = useParams();
+  const { currentUser } = useSelector((state) => state.user);
+  const { country, loading, countryError } = useGetCountry(countryName);
+  const navigate = useNavigate()
+  const [flight, setFlight] = useState()
+  const [iata, setIata] = useState()
+
+  const currentDate = new Date();
+  const tomorrowDate = new Date();
+  tomorrowDate.setDate(currentDate.getDate() + 1);
+  const tomorrow = tomorrowDate.toISOString().split('T')[0];
+
+  const handleChange = (e) => {
+    setIata(e.target.value)
+  }
+
+  const urlParams = new URLSearchParams
+  urlParams.set('fly_from', currentUser.user_iata || iata)
+  urlParams.set('fly_to', country.capital_iata)
+  urlParams.set('date_from', tomorrow)
+  urlParams.set('date_to', tomorrow)
+  urlParams.set('curr', 'PHP')
+  urlParams.set('adults', 1)
+  urlParams.set('children', 0)
+  urlParams.set('infants', 0)
+  urlParams.set('selected_cabins', 'M')
+  const filterQuery = urlParams.toString();
+
+  console.log(iata)
+  console.log(flight)
+  console.log(tomorrow)
+  console.log(filterQuery)
+
+  const fetchFlightData = async () => {
+    try {
+      const res = await fetch(`/api/flights/getFlight/${filterQuery}`);
+      const flightData = await res.json();
+      console.log(flightData)
+      if (flightData.success === false) {
+        // setLoading(false)
+        // setError(flightData.message)
+      }
+      setFlight(flightData)
+    } catch (error) {
+      // setError(error)
+      console.log(error)
+    }
+  }
+  if (currentUser.user_iata) {
+    fetchFlightData()
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchFlightData()
+  }
+
    
   return (
     <>
@@ -26,7 +81,7 @@ export default function Country() {
           <div className="flex mt-9">
             <div>
               <p className="text-6xl">
-                {country.country} 
+                {country.country}
                   <span className="text-2xl"> {country.continent_name}</span>
               </p>
               <p className="mt-9">
@@ -36,7 +91,30 @@ export default function Country() {
                 Start a plan
               </p>
             </div>
-            <p>Check the cheapest flights, add flights component here</p>
+            <div>
+              {flight ? 
+                <div>
+                  <p>Cheapest flight from your location is:</p>
+                  <p>{flight.price}</p>
+                </div>
+              :
+                <div>
+                  <p>Check out the cheapest flights</p>
+                  <form onSubmit={handleSubmit}>
+                    <label>From</label>
+                    <input 
+                      className='border border-black px-3 py-2 rounded-lg'
+                      type="text" 
+                      required
+                      placeholder='Where are you from?'  
+                      onChange={handleChange}
+                      value={iata}
+                    />
+                    <button className='bg-green-200 px-5 py-2'>Search</button>
+                  </form>
+                </div>
+              }
+            </div>
           </div>
         </div>
 
