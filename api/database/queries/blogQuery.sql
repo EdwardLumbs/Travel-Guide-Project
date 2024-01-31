594d09bd46906f59b27b3358cb8f1bdba1f9c0a0
Edward <programmeredward@gmail.com>
2024-01-31 15:21:25 +0800

    updated blog sql query

--- api/database/queries/blogQuery.sql
+++ api/database/queries/blogQuery.sql
@@ -2,7 +2,7 @@ CREATE TABLE blogs (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     title VARCHAR(255) NOT NULL,
-    place_tag VARCHAR(75) NOT NULL,
+    place_tag text[],
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     photo TEXT NOT NULL,
     content TEXT NOT NULL
