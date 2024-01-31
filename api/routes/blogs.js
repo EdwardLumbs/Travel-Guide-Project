da0f335e1ac4bc4ac89efc791db03f14677b5701
Edward <programmeredward@gmail.com>
2024-01-31 15:23:48 +0800

    Updated blogs API route

--- api/routes/blogs.js
+++ api/routes/blogs.js
@@ -8,8 +8,17 @@ dotenv.config();
 
 const router = express.Router();
 
-router.post('/create-post', verifyToken, (req, res, next) => {
-    
+router.post('/create-post', verifyToken, async (req, res, next) => {
+    const { user_id, title, place_tag, photo, content } = req.body
+
+    try {
+        await pool.query("INSERT INTO blogs (user_id, title, place_tag, photo, content) VALUES ($1, $2, $3, $4, $5)",
+            [user_id, title, place_tag, photo, content])
+            res.status(201).json("Blog Posted Successfully");
+
+    } catch (error) {
+        next(error);
+    }
 })
 
 
