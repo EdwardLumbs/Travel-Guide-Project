fc36b1f5cc083af4756946d736e127fcbea21987
Edward <programmeredward@gmail.com>
2024-02-17 13:52:41 +0800

    Updated error page UI

--- client/src/pages/404Page.jsx
+++ client/src/pages/404Page.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function NotFoundPage() {
   return (
-    <div>ERROR 404</div>
+    <div className="h-screen container flex flex-col mx-auto px-4">
+      <div className="h-full gap-4 flex flex-col justify-center items-center">
+        <h1 className="font-bold text-8xl">
+          404 Error
+        </h1>
+        <p className="text-lg">
+          Oops. Looks like you're trying to access a page that doesn't exist
+        </p>
+        <Link 
+          to={'/'}
+          className='mt-2 border px-6 py-2 rounded-full border-blue-800 bg-blue-800 text-white 
+            font-semibold hover:bg-white duration-300 hover:text-blue-800'
+        >
+          Go back
+        </Link>
+      </div>
+    </div>
   )
 }
